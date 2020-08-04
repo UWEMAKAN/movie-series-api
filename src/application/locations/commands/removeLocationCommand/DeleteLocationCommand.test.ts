ef9e71af6544aa1650f5fd6901b588561a962f9e
Uwem Akan Nkereuwem <uwemakan12@gmail.com>
2020-08-04 06:04:44 +0100

    comments commands added

--- src/application/locations/commands/removeLocationCommand/DeleteLocationCommand.test.ts
+++ src/application/locations/commands/removeLocationCommand/DeleteLocationCommand.test.ts
@@ -2,7 +2,7 @@ import DeleteLocationCommand from './DeleteLocationCommand';
 import * as mockEntities from '../../../../../tools/mockEntities';
 
 describe('DeleteLocationCommand', () => {
-  it('execute should create a new object of type Location and call locationRepository.add to save it', async () => {
+  it('execute should delete object locationRepository.remove', async () => {
     const location = mockEntities.locations[0];
     const command = new DeleteLocationCommand(mockEntities.locationRepository);
     await command.execute(location);
